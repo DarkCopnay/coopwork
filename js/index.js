407b0cf4d0e64c8b106bacafa6e4bf6aba67c7b2
Maksim <56311335+remix2021687@users.noreply.github.com>
2023-05-28 22:45:15 +0200

    add_step_1

--- js/index.js
+++ js/index.js
@@ -2,29 +2,33 @@ let daytext = document.getElementById("day");
 let hourstext = document.getElementById("hours");
 let minutestext = document.getElementById("minutes");
 let scendstext = document.getElementById("scends");
-let day = 15;
-let hours = 5;
-let minutes = 50;
-let scends = 1;
+let step_1_footer_progersbar_2 = document.getElementById("step_1_footer_progersbar");
 
-daytext.childNodes[1].innerHTML = day;
-hourstext.childNodes[1].innerHTML = hours;
-minutestext.childNodes[1].innerHTML = minutes;
-scendstext.childNodes[1].innerHTML = scends;
+function Time(day = 0, hours = 0, minutes = 0, scends = 0) {
+    daytext.childNodes[1].innerHTML = day;
+    hourstext.childNodes[1].innerHTML = hours;
+    minutestext.childNodes[1].innerHTML = minutes;
+    scendstext.childNodes[1].innerHTML = scends;
 
-let TimeInterval = setInterval(function() {
-    scendstext.childNodes[1].innerHTML = scends--;
-    if (scends == 0) {
-        scends = 59;
-        minutestext.childNodes[1].innerHTML = minutes--;
-        if (minutes == 0) {
-            minutes = 59;
-            hourstext.childNodes[1].innerHTML = hours--;
-            if (hours == 0) {
-                hours = 24;
-                daytext.childNodes[1].innerHTML = day--;
-                clearInterval(TimeInterval);
+    let TimeInterval = setInterval(function() {
+        scendstext.childNodes[1].innerHTML = scends--;
+        if (scends < 0) {
+            scends = 59;
+            minutestext.childNodes[1].innerHTML = minutes--;
+            if (minutes < 0) {
+                minutes = 59;
+                hourstext.childNodes[1].innerHTML = hours--;
+                if (hours < 0) {
+                    hours = 23;
+                    daytext.childNodes[1].innerHTML = day--;
+                    clearInterval(TimeInterval);
+                }
             }
         }
-    }
-}, 1000)
+    }, 1000)
+}
+
+// function ProgerBar()
+
+
+Time(18, 5, 30, 55);
